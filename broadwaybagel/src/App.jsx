25167559ad782a7a4bc951d92e2b245b983cd456
Cherch <b.cherchiglia@gmail.com>
2023-09-08 12:38:38 -0400

    browser router

--- broadwaybagel/src/App.jsx
+++ broadwaybagel/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -10,20 +9,24 @@ function App() {
     <>
       <div>
       <h1>Broadway Bagel & Deli</h1>
+      <p>
+        839 Broadway Brooklyn, NY 11206
+        <br />
+        PHONE: 718-490-5534
+      </p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <br />
         <a href="https://react.dev" target="_blank">
-          <img src="public/bodega_sando.png" className="logo react" alt="React logo" />
+          <img src="public/bodega_sando.png" className="logo sando" alt="React logo" />
         </a>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
