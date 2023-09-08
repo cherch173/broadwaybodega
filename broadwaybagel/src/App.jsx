79c940cced698e6ae8ec2f221a5369ad1e5fe06a
Cherch <b.cherchiglia@gmail.com>
2023-09-08 12:26:10 -0400

    sando

--- broadwaybagel/src/App.jsx
+++ broadwaybagel/src/App.jsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
+      <h1>Broadway Bagel & Deli</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="public/bodega_sando.png" className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
